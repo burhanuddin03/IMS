7ed522bce72ba21298ac081707b2d927ab413a3c
burhanuddin03 <44511968+burhanuddin03@users.noreply.github.com>
2022-05-23 20:59:40 +0500

    Update settings.py

--- adminpos/adminpos/settings.py
+++ adminpos/adminpos/settings.py
@@ -127,11 +127,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+# STATICFILES_DIRS = [
 
-os.path.join(BASE_DIR,'static'),
+# os.path.join(BASE_DIR,'static'),
 
-]
+# ]
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,"media")
 
